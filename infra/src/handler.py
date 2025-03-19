428309de73357a5aeefc9c35949c4bcdeec44824
shgigi <work.sagibns@gmail.com>
2025-03-19 12:45:51 +0200

    updated function with correct logic

--- infra/src/handler.py
+++ infra/src/handler.py
@@ -1,5 +1,9 @@
 import boto3
 import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 def get_environment_variables():
     queue_url = os.environ["QUEUE_URL"]
@@ -11,20 +15,19 @@ def lambda_handler(event, context):
     QUEUE_URL, TIMEOUT = get_environment_variables()
     sqs_client = boto3.client("sqs")
 
-    # 1. RecieveMessage
-    message = sqs_client.recieve_message(
-        queueUrl=QUEUE_URL,
-        MaxNumberOfMessages=1,
-        VisibilityTimeout=TIMEOUT
-    )
+    logger.info(f"EVENT: {event}")
+
+    logger.info(f"Recieved message from SQS {QUEUE_URL}")
+
     
     # 2. Create PR
 
 
     # 3. DeleteMessage
 
-    receipt_handle = message.get("Messages")[0].get("ReceiptHandle")
+    receipt_handle = event.get("Records")[0].get("receiptHandle")
     sqs_client.delete_message(
         QueueUrl=QUEUE_URL,
         ReceiptHandle=receipt_handle
     )
+    logger.info("Deleted message from queue")
